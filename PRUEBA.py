5b3016386b59af5c6b64086dcff2d784c9f4b6f7
abelBEDOYA <abelgonzalezbernad@gmail.com>
2023-01-31 10:01:39 +0100

    plot parabola

--- PRUEBA.py
+++ PRUEBA.py
@@ -1,8 +1,13 @@
 import numpy as np
 from clearml import Task
+import matplotlib.pyplot as plt
 task = Task.init(project_name="github_proyecto", task_name="my task 2")
             
 a = 5
 b =4
 c = 6/(a-b+1)
 
+x = np.linspace(1,30,1)
+y = x**2
+
+plt.plot(x, y)
