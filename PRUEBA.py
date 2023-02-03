76c096ac52fbf7e90bea6a2c7c09c9051d503e1f
abelBEDOYA <abelgonzalezbernad@gmail.com>
2023-02-03 09:10:17 +0100

    archivos varios de juguete

--- PRUEBA.py
+++ PRUEBA.py
@@ -1,8 +1,16 @@
 import numpy as np
 from clearml import Task
 import matplotlib.pyplot as plt
-task = Task.init(project_name="github_proyecto", task_name="my task 2")
+
 from modulo1 import plotear
+import cv2
+import time
+import random
+
+
+task_name_ = random.randint(1000,5000)
+task = Task.init(project_name="holaaaaa", task_name=str(task_name_))
+task.execute_remotely(queue_name='cola1')
 a = 5
 b = 4
 c = 6/(a-b+1)
@@ -12,20 +20,40 @@ y = f*x**2
 print('-----')
 print(task.id)
 print('-----')
+print('holaaaaaaa')
 
 
-plotear(x, y)
+
+    
+# plotear(x, y)
 # p=plt.plot(x, y, 'o')
 # plt.show()
 # plt.close()
 # task.get_logger().report_matplotlib_figure(title='mi primer plot', series='p', figure=p)
 
-task.get_logger().report_scalar(title='task', series='a', value=a, iteration=0)
-for i in range(1,10):
+# task.upload_artifact('pesos', 
+#     artifact_object=
+#         './pesos.pt')
+
+# task.upload_artifact('image', 
+#     artifact_object=
+#         './image1.jpg')
+
+# img = cv2.imread('./image1.jpg')
+# cv2.imshow('ploteo imagen', img)
+# cv2.waitKey(0)
+
+# task.get_logger().report_image(title='imageennn',local_path = './image1.jpg', series = 'bb') # series = img) #, value=img)
+
+# task.get_logger().report_scalar(title='task', series='a', value=a, iteration=0)
+for i in range(1,30):
     a = a +1
+    print(i)
+    time.sleep(1)
     task.get_logger().report_scalar(title='task', series='a', value=a, iteration=i**2-i+3)
 
 task.connect_configuration(
     name='yaml conseguidoooo',
     configuration='config.yaml'
 )
+task.close()
--- modulo1.py
+++ modulo1.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
 def plotear(x,y):
-    plt.plot(x, y, 'o')
-    plt.show()
-    plt.close()
+    # plt.plot(x, y, 'o')
+    # plt.show()
+    # plt.close()
+    pass
